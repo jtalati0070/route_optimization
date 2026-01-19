a43d7f46f945771694e59e455fb17a068dadde88
Jatin Talati <jatin_talati@Jatins-MacBook-Pro.local>
2026-01-19 14:54:01 +0530

    Added customer list near plano including lat and long.

---
+++ customer_data.py
@@ -0,0 +1,93 @@
+stores_near_pepsico_plano = [
+    {
+        "name": "Walmart Supercenter - Central Expy",
+        "address": "6001 Central Expy, Plano, TX 75023",
+        "chain": "Walmart",
+        "lat": 33.0389,
+        "lon": -96.6985
+    },
+    {
+        "name": "Walmart Supercenter - Coit Rd",
+        "address": "425 Coit Rd, Plano, TX 75075",
+        "chain": "Walmart",
+        "lat": 33.0423,
+        "lon": -96.7337
+    },
+    {
+        "name": "Walmart Supercenter - Ohio Dr",
+        "address": "8801 Ohio Dr, Plano, TX 75024",
+        "chain": "Walmart",
+        "lat": 33.0261,
+        "lon": -96.7699
+    },
+    {
+        "name": "Walmart Supercenter - Dallas Pkwy",
+        "address": "1700 Dallas Pkwy, Plano, TX 75093",
+        "chain": "Walmart",
+        "lat": 33.0470,
+        "lon": -96.7518
+    },
+    {
+        "name": "Walmart Neighborhood Market - Custer Rd",
+        "address": "3100 Custer Rd, Plano, TX 75075",
+        "chain": "Walmart",
+        "lat": 33.0375,
+        "lon": -96.7350
+    },
+    {
+        "name": "Walmart Neighborhood Market - Park Blvd",
+        "address": "3513 E Park Blvd, Plano, TX 75074",
+        "chain": "Walmart",
+        "lat": 33.0348,
+        "lon": -96.6657
+    },
+    {
+        "name": "Walmart Neighborhood Market - Preston Rd",
+        "address": "3312 Preston Rd, Plano, TX 75093",
+        "chain": "Walmart",
+        "lat": 33.0229,
+        "lon": -96.7081
+    },
+    {
+        "name": "Walmart Neighborhood Market - Independence Pkwy",
+        "address": "8040 Independence Pkwy, Plano, TX 75025",
+        "chain": "Walmart",
+        "lat": 33.0288,
+        "lon": -96.7335
+    },
+    {
+        "name": "Kroger Marketplace - 121 & Coit",
+        "address": "9617 Coit Rd, Plano, TX 75024",
+        "chain": "Kroger",
+        "lat": 33.0471,
+        "lon": -96.7161
+    },
+    {
+        "name": "Kroger - Los Rios",
+        "address": "4017 14th St, Plano, TX 75074",
+        "chain": "Kroger",
+        "lat": 33.0287,
+        "lon": -96.6746
+    },
+    {
+        "name": "Kroger - Custer Park",
+        "address": "2925 Custer Rd, Plano, TX 75075",
+        "chain": "Kroger",
+        "lat": 33.0415,
+        "lon": -96.7411
+    },
+    {
+        "name": "Kroger - Independence Pkwy",
+        "address": "7100 Independence Pkwy, Plano, TX 75025",
+        "chain": "Kroger",
+        "lat": 33.0174,
+        "lon": -96.7479
+    },
+    {
+        "name": "Kroger - Windhaven Plaza",
+        "address": "3305 Dallas Pkwy, Plano, TX 75093",
+        "chain": "Kroger",
+        "lat": 33.0453,
+        "lon": -96.7521
+    }
+]
